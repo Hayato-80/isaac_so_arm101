3b1433fb3f7e96148d16f4de7261fcdb4fb0cd73
Hayato-80 <hayato.kojima80@gmail.com>
2026-06-21 21:49:40 +0900

    Training done. Pose error reduced to 0.02 and mean reward improved.

--- src/isaac_so_arm101/tasks/reach/joint_pos_env_cfg.py
+++ src/isaac_so_arm101/tasks/reach/joint_pos_env_cfg.py
@@ -84,7 +84,8 @@ class SoArm101ReachEnvCfg(ReachEnvCfg):
         self.actions.arm_action = mdp.JointPositionActionCfg(
             asset_name="robot",
             joint_names=["shoulder_pan", "shoulder_lift", "elbow_flex", "wrist_flex", "wrist_roll"],
-            scale=0.5,
+            # scale=0.5,
+            scale = 1.0,
             use_default_offset=True,
         )
         # override command generator body
